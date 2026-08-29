85e5306538a6be577c9a1ca6847b5158d777728e
azwpayne <paynewu0719@gmail.com>
2026-08-30 02:18:07 +0800

    chore(fish): 统一 PATH 注入 homebrew/cargo/uv 等路径并清理空行
    使用 fish_add_path -g 显式纳管常用二进制目录，并补齐文件末尾换行

--- private_dot_config/private_fish/private_conf.d/00_env.fish
+++ private_dot_config/private_fish/private_conf.d/00_env.fish
@@ -3,11 +3,16 @@ complete --command kubecolor --wraps kubectl
 set -gx LANG zh_CN.UTF-8
 set -gx LC_ALL zh_CN.UTF-8
 
-
 set -gx EDITOR nvim
 set -gx VISUAL nvim
-
 
 set -gx HOMEBREW_NO_AUTO_UPDATE 1
 set -gx HOMEBREW_NO_INSTALL_CLEANUP 0
 set -gx HOMEBREW_NO_ENV_HINTS 1
+
+fish_add_path -g /opt/homebrew/bin \
+    /opt/homebrew/sbin \
+    /usr/local/bin \
+    $HOME/.local/bin \
+    $HOME/.cargo/bin \
+    $HOME/.local/share/uv/bin
