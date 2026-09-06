586175862f6e669e5149c45b91e4010989618f7d
azwpayne <paynewu0719@gmail.com>
2026-09-06 18:07:25 +0000

    refactor(zsh): use tilde expansion in chezc alias for consistency

--- private_dot_config/zsh/aliases.zsh
+++ private_dot_config/zsh/aliases.zsh
@@ -21,7 +21,7 @@ alias finder='open .'                            # Finder 打开当前目录
 alias iterm_ghostty='open -a ghostty "$PWD"'     # Ghostty 打开当前目录（单引号保证运行时取 $PWD）
 alias iterm='open -a iTerm "$PWD"'               # iTerm 打开当前目录
 
-alias chezc='code ${HOME}/.local/share/chezmoi'  # 编辑 chezmoi 源目录 (~/.local/share/chezmoi)
+alias chezc='code ~/.local/share/chezmoi'        # 编辑 chezmoi 源目录 (~/.local/share/chezmoi)
 alias chezdf='chezmoi diff'                      # 预览 chezmoi 变更 (apply 前必跑)
 alias chezap='chezmoi apply -v'                  # 应用 chezmoi 变更 (verbose)
 
